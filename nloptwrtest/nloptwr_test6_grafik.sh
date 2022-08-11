2d8a44967bce71d0be1632d7ddd4ba3d9b83e0e3
stopbystudent <stopbystudent@users.noreply.github.com>
2022-08-11 12:44:35 +0200

    Split misc directory
    The misc directory contained cmake files for this project as well as tools to
    generate graphics for one test case. The graphic tooling has been moved to the
    test directory to be close to its subjects of interest.

--- nloptwrtest/nloptwr_test6_grafik.sh
+++ nloptwrtest/nloptwr_test6_grafik.sh
@@ -4,7 +4,7 @@ cd ../build
 cmake .. 
 cmake --build . 
 ./nloptwrtest/nloptwr_test06 
-../misc/nloptwr_test6.plt 
+../nloptwrtest/nloptwr_test6.plt 
 xpdf nloptwr_test6.pdf 
-cd ../misc
+cd ../nloptwrtest/
 
